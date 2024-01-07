af19d7f8eabafef94e914cb99d34d49e70090085
Eren Tasci <erent4sci@gmail.com>
2024-01-07 22:04:41 +0300

    feat: making api call with redux-thunk

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -14,14 +14,16 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((state) => state.account);
 
   function handleDeposit() {
     if (!depositAmount) {
       return;
     }
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("");
   }
 
   function handleWithdrawal() {
@@ -66,7 +68,10 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {" "}
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
